3734eb8161573d93916f66276b3178334ddf4b7f
Chazz0606 <cklibrary2@qq.com>
2024-05-09 18:52:58 +0800

    core build

--- TimesWebsite/settings.py
+++ TimesWebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,7 +71,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'TimesWebsite.wsgi.application'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
--- magazines/models.py
+++ magazines/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Magazine(models.Model):
+    title = models.CharField(max_length=100)
+    upload_time = models.DateTimeField()
+    document = models.FileField(upload_to='static/documents/')
