b9d9573a46b61387b82be13ae9ec583f4a7079af
lightningstrike3045 <lightningstriked2007@gmail.com>
2024-05-13 10:49:37 +0800

    gocommitdie

--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class savedLibrary(models.model):
+    user_id = models.IntegerField()
+    saved_magazines = models.JSONField()
+    saved_count = models.IntegerField()
