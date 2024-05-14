ae1231e0bef1b4ad397c4cd8f4f178d81ce650e1
Yu Ming Law <ymlaw1125@yahoo.com>
2024-05-14 10:07:19 +0800

    changed files

--- TimesWebsite/settings.py
+++ TimesWebsite/settings.py
@@ -16,8 +16,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_URL = '/static/'
+STATIC_ROOT = ''
+STATICFILES_DIRS = ('static', )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -42,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "magazines.apps.MagazinesConfig",
     "authentication.apps.AuthenticationConfig",
+    "user.apps.UserConfig",
 ]
 
 MIDDLEWARE = [
@@ -85,7 +89,6 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'authentication.CustomUser'
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -119,8 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-MEDIA_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- authentication/models.py
+++ authentication/models.py
@@ -1,10 +1,4 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    favorites = models.JSONField(default=dict)
-
-    def __str__(self):
-        return self.username
