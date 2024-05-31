deb4d62778e7e754062719bb922a0764c7982ddd
Chazz0606 <cklibrary2@qq.com>
2024-05-31 11:22:47 +0800

    email login and favorites ManyToManyField

--- magazines/models.py
+++ magazines/models.py
@@ -8,7 +8,8 @@ class Magazine(models.Model):
     title = models.CharField(max_length=100)
     publisher = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
-    upload_time = models.DateTimeField()
+    upload_time = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
     document = models.FileField(upload_to='media/library/')
 
     def __str__(self):
